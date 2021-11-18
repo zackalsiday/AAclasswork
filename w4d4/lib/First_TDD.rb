436e1c98905616bc08773a6d21ee335903ed890d
zack alsiday <alsidayzack@gmail.com>
2021-11-18 14:55:32 -0800

    finished Q2

--- w4d4/lib/First_TDD.rb
+++ w4d4/lib/First_TDD.rb
@@ -7,3 +7,15 @@ def my_uniq(array)
     return newarray
 end
 
+def two_sum(array)
+    newArray = []
+    array.each_with_index do |num1,idx1|
+        array.each_with_index do |num2,idx2| 
+            newArray << [idx1,idx2] if num1 + num2 == 0 && idx2 > idx1 
+        end
+    end
+    newArray 
+end
+
+
+
--- w4d4/spec/First_TDD_spec.rb
+++ w4d4/spec/First_TDD_spec.rb
@@ -11,3 +11,9 @@ describe "#my_uniq" do
         expect(my_uniq([1,2,1,3,3])).to eq([1,2,3])
     end
 end
+
+describe "#two_sum" do 
+    it "returns pairs that sum to 0" do 
+        expect(two_sum([-1, 0, 2, -2, 1])).to eq([[0, 4], [2, 3]]) 
+    end
+end
