fc90301c822a2ffc5a241d680035e24af120aca8
Zyaire <89179829+zlevesque@users.noreply.github.com>
2021-11-19 12:08:15 -0800

    Completed phase 1

--- w4d5/execution_time.rb
+++ w4d5/execution_time.rb
@@ -1,1 +1,27 @@
-puts "hello"
+#puts "hello"
+
+# Given a list of integers find the smallest number in the list.
+
+# Example:
+
+def my_min(arr)
+  arr.inject do |acc, i|
+    count = acc
+    if i < count #0 < 3 
+      count = i
+    end
+    count
+  end
+end
+
+# def my_min(arr)
+#   arr.inject do |acc, i|
+#     if i < acc 
+#       i 
+#     else
+#       acc
+#     end
+#   end
+# end
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)  # =>  -5
