a965aec34e2ce3e9c461589905a0bd3ecb934246
zack alsiday <alsidayzack@gmail.com>
2021-12-03 15:18:35 -0800

    finsihed first section

--- w6d5/ninty_nine_cats/app/models/cat.rb
+++ w6d5/ninty_nine_cats/app/models/cat.rb
@@ -1,7 +1,13 @@
 require 'date'
 require 'action_view'
 class Cat < ApplicationRecord
-    include ActionView DateHelpers
-    validates :name, :birth_date, :color, :description, presense: true
+    include ActionView::Helpers::DateHelper
+    validates :name, :birth_date, :description, presense: true
     validates :sex, presense: true, :inclusion=> {:in => ['M', 'F']}
+# validates :sex, inclusion: %w(M F)
+    validates :color, presense: true , inclusion: {:in => ['Brown', 'White', 'Black', 'Orange', 'Grey'] 
+
+    def age
+       (Time.zone.now - birth_date.to_time / 1.year.seconds).floor
+    end
 end
