fed783b1df5e87cf4295daca07aafa9454f3145a
zack alsiday <alsidayzack@gmail.com>
2021-12-08 16:36:44 -0800

    finished reset session

--- w7d3/goal_app/spec/models/user_spec.rb
+++ w7d3/goal_app/spec/models/user_spec.rb
@@ -59,8 +59,17 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "reset_session_token!" do 
-    
+ describe 'session token' do
+   let!(:user) { FactoryBot.create(:user)}
+    it 'assigns a session_token if one is not given' do
+      expect(user.session_token).not_to be_nil
+    end
+
+    it 'resets a session token on a user' do
+      old_session_token = user.session_token
+      new_session_token = user.reset_session_token!
+      expect(old_session_token).not_to eq(new_session_token)
+    end
   end
 
 end
