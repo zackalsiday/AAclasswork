07a882f26605aabd5fc745b7d7f6ad1c55f4f1a8
zack alsiday <alsidayzack@gmail.com>
2021-12-17 14:22:45 -0800

    start project

---
+++ w7d5/arguments.js
@@ -0,0 +1,18 @@
+// function  sum() {
+//     let sum = 0;
+//     for (i = 0; i < arguments.length; i++) {
+//         sum += arguments[i]
+//     }
+//     return sum 
+// }
+
+// function  sum(...arr) {
+//     let sum = 0;
+//     for (i = 0; i < arr.length; i++) {
+//         sum += arr[i]
+//     }
+//     return sum 
+// }
+
+// console.log(sum(1, 2, 3, 4)) //=== 10;
+// console.log(sum(1, 2, 3, 4, 5))// === 15;
