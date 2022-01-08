a2e41189d58a3590f0c6a4cf08b8d11de8f1ed1c
zack alsiday <alsidayzack@gmail.com>
2022-01-07 17:02:48 -0800

    finsihed clock

--- w10d5/Widget/frontend/clock.jsx
+++ w10d5/Widget/frontend/clock.jsx
@@ -5,7 +5,7 @@ class Clock extends React.Component{
         super(props)
         this.state = { date: new Date()}; 
         this.tick = this.tick.bind(this);
-
+        this.time = setInterval(this.tick, 1000)
     }
 
     tick() {
@@ -16,12 +16,27 @@ class Clock extends React.Component{
     componentDidMount(){
        
         setInterval(this.tick, 1000)
-    
+   
     }
 
+    compnentWillUnMount(){
+        clearInterval(this.time)
+    }
+
+    
+
     render(){
+        const day = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat']
+        const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        const today = day[this.state.date.getDay()] + ' ,' + month[this.state.date.getMonth()] + ' ,' + this.state.date.getDate()
+        const currentTime = this.state.date.getHours() + ":" + this.state.date.getMinutes() + ":" + this.state.date.getSeconds()
+       return <div>
+           <h1>Clock</h1>
+           <h3>Time:{currentTime}</h3>
+           <h3> Date: {today }</h3>
+            </div>
+             
         
-       return <h1>Clock</h1>
         
     }
 
