cb798d236580d7678d4d0cb6cb56ac390f60aeac
kevinledev <kevinle.developer@gmail.com>
2022-03-04 15:23:49 -0800

    making clock

--- widgets/widgets/frontend/clock.jsx
+++ widgets/widgets/frontend/clock.jsx
@@ -3,13 +3,31 @@ import React from "react";
 
 class Clock extends React.Component {
     constructor(props){
-        super(props)
-        this.state = {time: new Date()}
+        super(props);
+        this.state = {time: new Date()};
+
+        this.tick = this.tick.bind(this);
     }
+
+    tick() {
+        this.setState({ time: new Date() }); 
+    }
+
+    componentDidMount() {
+        setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.tick)
+    }
+
+
     render() {
         return (
-            
-            <div>{console.log(this.state.time)}</div>
+            <div>
+            <h1>Clock</h1>
+            <div>{this.state.time.getHours()%12}:{this.state.time.getMinutes()}:{this.state.time.getSeconds()}</div>
+            </div>
         )
     }
 }
