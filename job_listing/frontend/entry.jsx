6f2864364b33d7de1393cd518f92fc646da6089c
zack alsiday <alsidayzack@gmail.com>
2022-03-07 23:59:25 -0800

    done

--- job_listing/frontend/entry.jsx
+++ job_listing/frontend/entry.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import selectLocation from './actions';
 import Widget from './components/widget';
 import store from './store';
+import reducer from './reducer'
 
 document.addEventListener("DOMContentLoaded", function(){
+  window.selectLocation = selectLocation
+ 
+ 
+  window.reducer = reducer
   ReactDOM.render(<Widget store={store} />, document.getElementById('root'));
 });
--- job_listing/frontend/reducer.js
+++ job_listing/frontend/reducer.js
@@ -4,7 +4,12 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch (action.type) {
+      case 'SWITCH_LOCATION':
+          return state.city = action.city;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
